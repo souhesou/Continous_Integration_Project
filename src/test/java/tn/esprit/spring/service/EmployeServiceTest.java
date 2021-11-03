0a6bc03ce14d9eac4d31af5840dc0ae40ea6169a
Souha <souha.saada@esprit.tn>
2021-11-03 08:56:59 +0000

    commit souu

--- src/test/java/tn/esprit/spring/service/EmployeServiceTest.java
+++ src/test/java/tn/esprit/spring/service/EmployeServiceTest.java
@@ -24,7 +24,7 @@ public class EmployeServiceTest {
 	@Test
 	public void testRetrieveAllEmploye() {
 		List<Employe> listEmployes = es.retrieveAllEmploye();
-		Assert.assertEquals(6, listEmployes.size());
+		Assert.assertEquals(7, listEmployes.size());
 	}
 
 	@Test
@@ -38,7 +38,7 @@ public class EmployeServiceTest {
 	@Test
 	public void testModifyEmploye() throws ParseException   {
 		
-		Employe e = new Employe(10L,"Sousouuuu", "Saada", "souhei123.saada@esprit.tn", "123", Role.ADMINISTRATEUR);
+		Employe e = new Employe(12L,"Sousouuuu", "Saada", "souhei123.saada@esprit.tn", "123", Role.ADMINISTRATEUR);
 		Employe employeModified = es.updateEmploye(e) ;
 		Assert.assertEquals(e.getNom(), employeModified.getNom());
 	}
@@ -46,8 +46,8 @@ public class EmployeServiceTest {
 	
 	@Test
 	public void testDeleteEmploye() {
-		es.remove("10");
-		Assert.assertNull(es.retrieveEmploye("10"));
+		es.remove("12");
+		Assert.assertNull(es.retrieveEmploye("12"));
 		
 		
 	}
